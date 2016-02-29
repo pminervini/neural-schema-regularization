2b5427b37b5347ebddb541b76600d2aa94b8775b
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2016-02-29 04:14:54 +0100

    update

---
+++ data/music/tools/download.sh
@@ -0,0 +1,3 @@
+wget -c http://downloads.dbpedia.org/2015-04/core-i18n/en/mappingbased-properties_en.nt.bz2
+wget -c http://downloads.dbpedia.org/2015-04/core-i18n/en/mappingbased-properties-errors-unredirected_en.nt.bz2
+wget -c http://downloads.dbpedia.org/2015-04/core-i18n/en/specific-mappingbased-properties_en.nt.bz2
