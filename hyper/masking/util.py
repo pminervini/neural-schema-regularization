97d52e14474548aa2fd17d8c1972619ff0771936
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2016-06-19 20:19:29 +0200

    update

--- hyper/masking/util.py
+++ hyper/masking/util.py
@@ -10,9 +10,12 @@ import logging
 
 def create_mask(nb_items, embedding_size, mask_ranges):
     assert nb_items == mask_ranges.shape[0]
+
     mask = np.zeros((nb_items, embedding_size), dtype=K.floatx())
+
     for i in range(mask_ranges.shape[0]):
         mask[i, mask_ranges[i, 0]:mask_ranges[i, 1]] = 1.
+
     return mask
 
 
