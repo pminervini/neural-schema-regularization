4361c8d607cc701645372369fd449046a9973c60
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2016-06-22 15:11:54 +0200

    some preliminary tests for implementing Knowledge Vault's ER-MLP model

--- tests/hyper/test_models.py
+++ tests/hyper/test_models.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
-from keras.layers.core import Merge
+from keras.layers.core import Merge, Reshape
 
 import hyper.layers.core
 from hyper.constraints import norm
@@ -14,15 +14,14 @@ import unittest
 
 
 class TestModels(unittest.TestCase):
+    _ITERATIONS = 8
 
     def setUp(self):
-        pass
+        self.rs = np.random.RandomState(1)
 
     def test_translating_embeddings_inference(self):
-        rs = np.random.RandomState(1)
-
-        for _ in range(32):
-            W_pred, W_emb = rs.random_sample((3, 10)), rs.random_sample((3, 10))
+        for _ in range(self._ITERATIONS):
+            W_pred, W_emb = self.rs.random_sample((3, 10)), self.rs.random_sample((3, 10))
 
             predicate_encoder = Sequential()
             entity_encoder = Sequential()
@@ -67,10 +66,8 @@ class TestModels(unittest.TestCase):
             self.assertTrue(abs(y[0, 0] - expected) < 1e-6)
 
     def test_translating_embeddings_learning(self):
-        rs = np.random.RandomState(1)
-
-        for _ in range(2):
-            W_pred, W_emb = rs.random_sample((3, 10)), rs.random_sample((3, 10))
+        for _ in range(self._ITERATIONS):
+            W_pred, W_emb = self.rs.random_sample((3, 10)), self.rs.random_sample((3, 10))
 
             predicate_encoder = Sequential()
             entity_encoder = Sequential()
@@ -81,7 +78,6 @@ class TestModels(unittest.TestCase):
             norm_constraint = norm(m=1.5, axis=1)
             entity_layer = Embedding(input_dim=3, output_dim=10, input_length=None, weights=[W_emb],
                                      W_constraint=norm_constraint)
-
             entity_encoder.add(entity_layer)
 
             model = Sequential()
@@ -121,6 +117,60 @@ class TestModels(unittest.TestCase):
             self.assertTrue(abs(np.linalg.norm(normalized_embeddings[1, :]) - 1.5) < 1e-6)
             self.assertTrue(abs(np.linalg.norm(normalized_embeddings[2, :]) - 1.5) < 1e-6)
 
+    def test_embedding_concatenation(self):
+        for _ in range(self._ITERATIONS):
+            W_emb = self.rs.random_sample((3, 10))
+
+            entity_encoder = Sequential()
+
+            entity_layer = Embedding(input_dim=3, output_dim=10, input_length=2, weights=[W_emb])
+            entity_encoder.add(entity_layer)
+
+            reshape_layer = Reshape(target_shape=(20,))
+            entity_encoder.add(reshape_layer)
+
+            entity_encoder.compile(loss='binary_crossentropy', optimizer='adagrad')
+
+            M = W_emb[1:, :]
+
+            Xe = np.array([[1, 2]])
+            y = entity_encoder.predict([Xe], batch_size=1)[0]
+
+            for i in range(M.shape[0]):
+                for j in range(M.shape[1]):
+                    self.assertTrue(abs(M[i, j] - y[(M.shape[1] * i) + j]) < 1e-8)
+
+    def test_embedding_concatenation_merge(self):
+        for _ in range(self._ITERATIONS):
+            W_pred, W_emb = self.rs.random_sample((3, 10)), self.rs.random_sample((3, 10))
+
+            predicate_encoder = Sequential()
+            entity_encoder = Sequential()
+
+            predicate_layer = Embedding(input_dim=3, output_dim=10, input_length=1, weights=[W_pred])
+            predicate_encoder.add(predicate_layer)
+
+            entity_layer = Embedding(input_dim=3, output_dim=10, input_length=2, weights=[W_emb])
+            entity_encoder.add(entity_layer)
+
+            reshape_layer = Reshape(target_shape=(1, 20))
+            entity_encoder.add(reshape_layer)
+
+            model = Sequential()
+            merge_layer = Merge([predicate_encoder, entity_encoder], mode='concat', concat_axis=-1)
+            model.add(merge_layer)
+
+            model.compile(loss='binary_crossentropy', optimizer='adagrad')
+
+            M = np.concatenate((W_pred[1:2, :], W_emb[1:, :]))
+
+            Xr = np.array([[1]])
+            Xe = np.array([[1, 2]])
+            y = model.predict([Xr, Xe], batch_size=1)[0]
+
+            for i in range(M.shape[0]):
+                for j in range(M.shape[1]):
+                    self.assertTrue(abs(M[i, j] - y[0, (M.shape[1] * i) + j]) < 1e-8)
 
 if __name__ == '__main__':
     unittest.main()
