bcc6fb17d4d580101089a7fc8aaf5a71e5b94c50
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2016-10-10 15:42:26 +0200

    script for producing the timings

---
+++ scripts/wn18/exp_wn18_timing_v2_gpu.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+import os.path
+
+
+def cartesian_product(dicts):
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+
+def summary(configuration):
+    kvs = sorted([(k, v) for k, v in configuration.items()], key=lambda e: e[0])
+    return '_'.join([('%s=%s' % (k, v)) for (k, v) in kvs])
+
+
+def to_command(c):
+    command = "PYTHONPATH=. ./bin/hyper-cli.py" \
+              " --train data/wn18/wordnet-mlj12-train.txt" \
+              " --epochs %s" \
+              " --optimizer %s" \
+              " --lr %s" \
+              " --batches %s" \
+              " --model %s" \
+              " --similarity %s" \
+              " --margin %s" \
+              " --entity-embedding-size %s" \
+              % (c['epochs'], c['optimizer'], c['lr'], c['batches'], c['model'], c['similarity'], c['margin'],
+                 c['embedding_size'])
+    return command
+
+
+def to_logfile(c, dir):
+    outfile = "%s/exp_wn18_timing_v2_cpu.%s.log" % (dir, summary(c))
+    return outfile
+
+
+hyperparameters_space = dict(
+    epochs=[1000],
+    optimizer=['adagrad'],
+    lr=[.1],
+    batches=[10],
+    model=['ComplEx'],
+    similarity=['dot'],
+    margin=[1],
+    embedding_size=[10, 20, 50, 100, 150, 200, 300, 400, 500, 600]
+)
+
+configurations = cartesian_product(hyperparameters_space)
+
+dir = 'logs/exp_wn18_timing_v2/'
+
+for c in configurations:
+    logfile = to_logfile(c, dir)
+
+    completed = False
+    if os.path.isfile(logfile):
+        with open(logfile, 'r') as f:
+            content = f.read()
+            completed = '### MICRO (test filtered)' in content
+
+    if not completed:
+        line = '%s >> %s 2>&1' % (to_command(c), logfile)
+        print(line)
